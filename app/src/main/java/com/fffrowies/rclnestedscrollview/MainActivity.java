c030ac08583a6a1ed7bf333dda01dcc2724e3558
fffrowies <fernandorowies.saxo@gmail.com>
2019-01-19 13:39:44 -0300

    Initial commit

---
+++ app/src/main/java/com/fffrowies/rclnestedscrollview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fffrowies.rclnestedscrollview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
